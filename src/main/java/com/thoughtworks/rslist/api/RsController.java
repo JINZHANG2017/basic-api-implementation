0ab41989d6486ee7776fd311dec0245f75317953
JINZHANG2017 <zj574406254@hotmail.com>
2020-09-15 22:00:15 +0800

    [ZhangJin] feat: add test for add one re

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,16 +1,22 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<String> rsList = initRsList();
+
+  private List<String> initRsList(){
+    List<String> tempRsList=new ArrayList<>();
+    tempRsList.add("第一条事件");
+    tempRsList.add("第二条事件");
+    tempRsList.add("第三条事件");
+    return tempRsList;
+  }
 
   @GetMapping("/rs/list")
   public String getList(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
@@ -23,5 +29,10 @@ public class RsController {
   @GetMapping("/rs/{id}")
   public String getOneRs(@PathVariable Integer id){
     return rsList.get(id-1);
+  }
+
+  @PostMapping("/rs/event")
+  public void postOneRs(@RequestBody String str){
+    rsList.add(str);
   }
 }
