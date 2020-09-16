70dc6c385d3d6dd021ea4bfef509f6ce7bfbb6d1
JINZHANG2017 <zj574406254@hotmail.com>
2020-09-16 19:55:22 +0800

    [ZhangJin] feat: add test for age less than 18

--- src/main/java/com/thoughtworks/rslist/dto/UserDto.java
+++ src/main/java/com/thoughtworks/rslist/dto/UserDto.java
@@ -3,10 +3,7 @@ package com.thoughtworks.rslist.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
@@ -32,6 +29,7 @@ public class UserDto {
     private String gender;
     @NotNull
     @Max(100)
+    @Min(18)
     private Integer age;
     private String email;
     @NotEmpty
